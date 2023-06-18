6a4f426909bceedbb4c85be03fcc1ddaa07d8ca5
sarbas33 <sarbas33@gmail.com>
2023-06-18 12:52:03 +0530

    Implement header for the portfolio website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+
+  openSkillsPage(){
+
+  }
+  openWorkPage(){
+    
+  }
+  openEducationPage(){
+    
+  }
+  openContactPage(){
+    
+  }
 }
